499fdc798689c512eaab45df7a8e3b69f40fbb7b
Tegan Jennings <teganjennings1@hotmail.com>
2024-12-28 20:17:16 +0000

    adding context for shopping cart

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import { useState } from "react";
 import Header from "./components/Header.jsx";
 import Shop from "./components/Shop.jsx";
 import Product from "./components/Product.jsx";
+import { CartContext } from "./store/shopping-cart-context.jsx";
 
 import { DUMMY_PRODUCTS } from "./dummy-products.js";
+import Cart from "./components/Cart.jsx";
 
 function App() {
 	const [shoppingCart, setShoppingCart] = useState({
@@ -69,17 +71,19 @@ function App() {
 
 	return (
 		<>
-			<Header
-				cart={shoppingCart}
-				onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
-			/>
-			<Shop>
-				{DUMMY_PRODUCTS.map((product) => (
-					<li key={product.id}>
-						<Product {...product} onAddToCart={handleAddItemToCart} />
-					</li>
-				))}
-			</Shop>
+			<CartContext.Provider value={{ items: [] }}>
+				<Header
+					cart={shoppingCart}
+					onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
+				/>
+				<Shop>
+					{DUMMY_PRODUCTS.map((product) => (
+						<li key={product.id}>
+							<Product {...product} onAddToCart={handleAddItemToCart} />
+						</li>
+					))}
+				</Shop>{" "}
+			</CartContext.Provider>
 		</>
 	);
 }
